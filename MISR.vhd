4a336540c05e9e8d47988cf59c0251cb8d2289b1
Your Name <you@example.com>
2019-11-20 17:17:38 -0500

    MISR spaces to tabs added end process statement

--- MISR.vhd
+++ MISR.vhd
@@ -34,29 +34,29 @@ entity MISR is
 end MISR;
 
 architecture Behavioral of MISR is
-    signal xnorn : std_logic_vector(7 downto 0) := "00000001";  --DEFAULT TO 1 because 0 is a NO NO
-begin
-    process (clk) is
-    begin
-        if (rising_edge(clk)) then
-            if rst = '0' then
-                xnorn <= "00000001"; --DEFAULT TO 1 because 0 is a NO NO
-            else
-                if enable = '1' then
-                    xnorn(0) <=    mult_r(0) xnor xnorn(3) xnor xnorn(4) xnor xnorn(5) xnor xnorn(7); --TAPS
-                    xnorn(1) <=    mult_r(1) xnor xnorn(0); --DFF with xnor 
-                    xnorn(2) <=    mult_r(2) xnor xnorn(1); --DFF with xnor 
-                    xnorn(3) <=    mult_r(3) xnor xnorn(2); --DFF with xnor 
-                    xnorn(4) <=    mult_r(4) xnor xnorn(3); --DFF with xnor 
-                    xnorn(5) <=    mult_r(5) xnor xnorn(4); --DFF with xnor 
-                    xnorn(6) <=    mult_r(6) xnor xnorn(5); --DFF with xnor 
-                    xnorn(7) <=    mult_r(7) xnor xnorn(6); --DFF with xnor 
-                else
-                    xnorn <= xnorn; --HOLD
-                end if;
-            end if;
-        end if;
-    process;
-signature <= xnorn;
+	signal xnorn : std_logic_vector(7 downto 0) := "00000001";  --DEFAULT TO 1 because 0 is a NO NO
+	begin
+		process (clk) is
+		begin
+			if (rising_edge(clk)) then
+				if rst = '0' then
+					xnorn <= "00000001"; --DEFAULT TO 1 because 0 is a NO NO
+				else
+				if enable = '1' then
+					xnorn(0) <=    mult_r(0) xnor xnorn(3) xnor xnorn(4) xnor xnorn(5) xnor xnorn(7); --TAPS
+					xnorn(1) <=    mult_r(1) xnor xnorn(0); --DFF with xnor 
+					xnorn(2) <=    mult_r(2) xnor xnorn(1); --DFF with xnor 
+					xnorn(3) <=    mult_r(3) xnor xnorn(2); --DFF with xnor 
+					xnorn(4) <=    mult_r(4) xnor xnorn(3); --DFF with xnor
+					xnorn(5) <=    mult_r(5) xnor xnorn(4); --DFF with xnor 
+					xnorn(6) <=    mult_r(6) xnor xnorn(5); --DFF with xnor 
+					xnorn(7) <=    mult_r(7) xnor xnorn(6); --DFF with xnor 
+				else
+					xnorn <= xnorn; --HOLD
+				end if;
+			end if;
+		end if;
+	end process;
+	signature <= xnorn;
 end Behavioral;
 
