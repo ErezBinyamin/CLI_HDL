17d3bf169a9626bd6f0952b1c9315676eafa66d1
Your Name <you@example.com>
2019-11-20 18:12:27 -0500

    bad ubuntu run code

--- ubuntu_run.sh
+++ ubuntu_run.sh
@@ -1,5 +1,38 @@
 #!/bin/bash
 SIM_DIR=sim
 SRC_DIR=work
+#TST_BCH=tst/MultiplierTB.vhd
+SIM_FILE=sim
+GTK_FILE=gtk.vcdgz
+STOP_TIME=500ns
 
-ghdl -i --ieee=synopsys --warn-no-vital-generic --workdir=${SIM_DIR} --work=${SRC_DIR} ${SRC_DIR}/*.vhd
+# Code from
+# http://www.armadeus.org/wiki/index.php?title=How_to_make_a_VHDL_design_in_Ubuntu/Debian
+
+simulate() {
+	# 1. Syntax check
+	ghdl -i --ieee=synopsys --warn-no-vital-generic --workdir=${SIM_DIR} --work=${SRC_DIR} ${SRC_DIR}/*.vhd
+	# 2. Compile
+	if [ $? -eq 0 ]
+	then
+		ghdl -m --ieee=synopsys --warn-no-vital-generic --workdir=${SIM_DIR} --work=${SRC_DIR} ${SIM_FILE}
+	else
+		return 1
+	fi
+	# 3. Generate simulation
+	if [ $? -eq 0 ]
+	then
+		./${SIM_FILE} --stop-time=${STOP_TIME} --vcdgz=${GTK_FILE}
+	else
+		return 1
+	fi
+	# 4. Run simulation
+	if [ $? -eq 0 ]
+	then
+		gunzip --stdout ${GTK_FILE} | gtkwave --vcd
+	else
+		return 1
+	fi
+}
+
+simulate ${@}
