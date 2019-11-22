fec67a1537b0b6c279f18e12ca7cb7b6d6fb681c
Your Name <you@example.com>
2019-11-22 10:58:01 -0500

    Simulation of MAC fails

--- tst/MAC_TB.vhd
+++ tst/MAC_TB.vhd
@@ -7,73 +7,44 @@ library IEEE;
 use IEEE.Std_logic_1164.all;
 use IEEE.Numeric_Std.all;
 
-entity Full_Adder_Nbit_tb is
+entity MAC_tb is
 end;
 
-architecture bench of Full_Adder_Nbit_tb is
+architecture bench of MAC_tb is
 
   constant N : INTEGER := 4;
-  component Full_Adder_Nbit
-       port
+  component MAC
+      port
        (
-            A     :   in   STD_LOGIC_VECTOR(N-1 downto 0);
-            B     :   in   STD_LOGIC_VECTOR(N-1 downto 0);
-            Cin   :   in   STD_LOGIC;
-            Sum   :   out  STD_LOGIC_VECTOR(N-1 downto 0);
-            Cout  :   out  STD_LOGIC
+            A        :   in     STD_LOGIC_VECTOR(n-1 downto 0);
+            B        :   in     STD_LOGIC_VECTOR(n-1 downto 0);
+            mac_out  :   out    STD_LOGIC_VECTOR((2*n) downto 0)
        );
   end component;
 
-  signal A   : STD_LOGIC_VECTOR(N-1 downto 0) := (others => '0');
-  signal B   : STD_LOGIC_VECTOR(N-1 downto 0) := (others => '0');
-  signal Cin : STD_LOGIC                      := '0';
-  signal Cout: STD_LOGIC                      := '0';
-  signal Sum : STD_LOGIC_VECTOR(N-1 downto 0) := (others => '0');
+  signal A: STD_LOGIC_VECTOR(n-1 downto 0);
+  signal B: STD_LOGIC_VECTOR(n-1 downto 0);
+  signal mac_out: STD_LOGIC_VECTOR((2*n) downto 0) ;
 
-  constant CLK_PERIOD : TIME := 10 ns;
 begin
 
-  uut: Full_Adder_Nbit
- 	  --generic map( N => N );
-	  port map ( A    => A,
-                   B    => B,
-                   Cin  => Cin,
-                   Sum  => Sum,
-                   Cout => Cout );
+  -- Insert values for generic parameters !!
+  uut: MAC
+              port map ( A       => A,
+                         B       => B,
+                         mac_out => mac_out );
 
   stimulus: process
   begin
+  
+    -- Put initialisation code here
+    A <= (others => '0');
+    B <= (others => '0');
 
-	  -- Add everything
-	  Cin <= '0';
-	  for i in 0 to 15 loop
-	  	for j in 0 to 15 loop
-		  	wait for CLK_PERIOD;
-			A <= STD_LOGIC_VECTOR(TO_UNSIGNED(j, A'length));
-		  	wait for CLK_PERIOD;
-	  	end loop;
-		B <= STD_LOGIC_VECTOR(TO_UNSIGNED(i, B'length));
-	  end loop;
+    -- Put test bench stimulus code here
 
-	  -- Reset
-	  wait for CLK_PERIOD;
-	  A <= (others => '0');
-	  B <= (others => '0');
-	  wait for CLK_PERIOD;
-
-	  -- Subtract everything
-	  Cin <= '1';
-	  for i in 0 to 15 loop
-	  	for j in 0 to 15 loop
-		  	wait for CLK_PERIOD;
-			A <= STD_LOGIC_VECTOR(TO_UNSIGNED(j, A'length));
-		  	wait for CLK_PERIOD;
-	  	end loop;
-		B <= STD_LOGIC_VECTOR(TO_UNSIGNED(i, B'length));
-	  end loop;
-
-  	  wait;
-end process;
+    wait;
+  end process;
 
 
 end;
---
+++ work/MAC.vhd
@@ -0,0 +1,116 @@
+----------------------------------------------------------------------------------
+-- Company : RIT
+-- Engineer: Erez Binyamin
+--
+-- Create Date:    20:20:52 10/03/2017
+-- Design Name:
+-- Module Name:    MAC - Structural
+-- Project Name:
+-- Target Devices:
+-- Tool versions:
+-- Description:
+--
+-- Dependencies: Full_Adder_Nbit
+--
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+-----------------------------------------------------------
+-- *                     MAC UNIT                      * -- 
+-----------------------------------------------------------
+-- *         A                   B            N-1      * --   
+-- *         |                   |                     * --                      
+-- *        +---------------------+                    * --    
+-- *         \      Multiply     /                     * --  
+-- *          +-----------------+                      * --    
+-- *                   |                               * --     
+-- *                 Product                (2*N)-1    * --   
+-- *                   |_______                        * --          
+-- *    ________               |                       * --         
+-- *    |      |               |                       * --            
+-- *    |   +---------------------+                    * --            
+-- *    |    \       Add         /                     * --            
+-- *    |     +-----------------+                      * --                  
+-- *    |              |                               * --            
+-- *    |             Sum                      2N      * --         
+-- *    |              |                               * --                
+-- *    |    +---------------------+                   * --               
+-- *    +----|       RegMem        |           2N      * --              
+-- *         +---------------------+                   * --            
+-- *                   |                               * --             
+-- *                 MacOut                    2N      * --               
+-----------------------------------------------------------
+
+
+entity MAC is
+    generic (n : integer := 4);
+    port
+     (
+          A        :   in     STD_LOGIC_VECTOR(n-1 downto 0);
+          B        :   in     STD_LOGIC_VECTOR(n-1 downto 0);
+          mac_out  :   out    STD_LOGIC_VECTOR((2*n) downto 0)
+     );
+end MAC;
+
+architecture Behave of MAC is
+	signal Product : STD_LOGIC_VECTOR(((2*n)-1) downto 0) := (others => '0');
+	signal Sum     : STD_LOGIC_VECTOR((2*n) downto 0)     := (others => '0');
+	signal Cout    : STD_LOGIC                            := '0';
+	signal RegMem  : STD_LOGIC_VECTOR((2*n) downto 0)     := (others => '0');
+begin
+	Product <= (SIGNED(A) * SIGNED(B));
+	Sum     <= SIGNED(Product) + SIGNED(RegMem);
+	RegMem  <= Sum;
+	mac_out <= RegMem;
+end Behave;
+
+-- architecture Structural of Multiplier is
+-- 
+-- 	Product STD_LOGIC_VECTOR(((2*n)-1) downto 0);
+-- 	Sum STD_LOGIC_VECTOR((2*n) downto 0);
+-- 	Cout STD_LOGIC;
+-- 	RegMem STD_LOGIC_VECTOR((2*n) downto 0);
+-- 
+-- begin
+-- 
+--     Mult : entity work.Full_Adder_Nbit
+--     generic map (N => n)
+--     port map
+--     (
+--         A       =>  A,
+--         B       =>  B,
+--         Product =>  Product
+--     );
+-- 
+--     Add : entity work.Full_Adder_Nbit
+--     generic map (N => ((2*N)+1))
+--     port map
+--     (
+--         A    =>    RegMem,
+--         B    =>    '0' & Product,
+--         Cin  =>    '0',
+--         Sum  =>    Sum,
+--         Cout =>    Cout
+--     );
+-- 
+--     RegMem <= Sum;
+-- 
+--     mac_out <= RegMem;
+-- 
+-- end Structural;
+-- 
