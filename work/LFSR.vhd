dd889180938e09da65d7440a9145f2182db54fd9
Your Name <you@example.com>
2019-12-03 12:32:28 -0500

    Made LFSR 16 bit

--- work/LFSR.vhd
+++ work/LFSR.vhd
@@ -31,12 +31,12 @@ entity LFSR is
             rst     : in std_logic;
             enable  : in std_logic;
     --OUTPUTS
-            bit_p   : out std_logic_vector(7 downto 0)
+            bit_p   : out std_logic_vector(15 downto 0)
     );
 end LFSR;
 
 architecture Behavioral of LFSR is
-    signal xnorn : std_logic_vector(7 downto 0) := "00000001"; --DEFAULT TO 1 because 0 is a NO NO
+    signal xnorn : std_logic_vector(15 downto 0) := "0000000000000001"; --DEFAULT TO 1 because 0 is a NO NO
 begin
 
 --SYNCHRONOUS shift register
@@ -45,10 +45,10 @@ begin
     begin
             if (rising_edge(clk)) then
                 if rst = '0' then
-                    xnorn <= "00000001"; --RESET
+                    xnorn <= "0000000000000001"; --RESET
                 else
                     if enable = '1' then
-                        xnorn(0) <= xnorn(7) xnor xnorn(5) xnor xnorn(4) xnor xnorn(3); --TAPS
+                        xnorn(0) <= xnorn(15) xnor xnorn(13) xnor xnorn(12) xnor xnorn(10); --TAPS
                         xnorn(1) <= xnorn(0); --DFF
                         xnorn(2) <= xnorn(1); --DFF
                         xnorn(3) <= xnorn(2); --DFF
@@ -56,6 +56,14 @@ begin
                         xnorn(5) <= xnorn(4); --DFF
                         xnorn(6) <= xnorn(5); --DFF
                         xnorn(7) <= xnorn(6); --DFF
+                        xnorn(8) <= xnorn(7); --DFF
+                        xnorn(9) <= xnorn(8); --DFF
+                        xnorn(10) <= xnorn(9); --DFF
+                        xnorn(11) <= xnorn(10); --DFF
+                        xnorn(12) <= xnorn(11); --DFF
+                        xnorn(13) <= xnorn(12); --DFF
+                        xnorn(14) <= xnorn(13); --DFF
+                        xnorn(15) <= xnorn(14); --DFF
                     else
                         xnorn <= xnorn;       --HOLD
                     end if;
