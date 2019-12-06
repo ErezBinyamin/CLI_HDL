e820457efcf3a706d216c47e7adb82a4c8f874f8
Your Name <you@example.com>
2019-12-05 23:02:05 -0500

    simulation runs, LFSR does not generate enough random numbers

--- work/LFSR.vhd
+++ work/LFSR.vhd
@@ -1,9 +1,9 @@
 -- AUTHOR: Erez Binyamin
 -- DESIGN NAME: LFSR.vhd
 -- SHORT DESCRIPTION:
---      An 8-bit Linear feedback shift register
+--      A 32-bit Linear feedback shift register
 --      For generating a random number sequence
---      as an input to an 4-bit multiplier.
+--      as an input to an 16-bit multiplier.
 --      (Generating the A and B inputs)
 -- For More Taps visit:
 --			http://courses.cse.tamu.edu/walker/csce680/lfsr_table.pdf
@@ -31,12 +31,12 @@ entity LFSR is
             rst     : in std_logic;
             enable  : in std_logic;
     --OUTPUTS
-            bit_p   : out std_logic_vector(15 downto 0)
+            bit_p   : out std_logic_vector(31 downto 0)
     );
 end LFSR;
 
 architecture Behavioral of LFSR is
-    signal xnorn : std_logic_vector(15 downto 0) := "0000000000000001"; --DEFAULT TO 1 because 0 is a NO NO
+    signal xnorn : std_logic_vector(31 downto 0) := "00000000000000000000000000000001"; --DEFAULT TO 1 because 0 is a NO NO
 begin
 
 --SYNCHRONOUS shift register
@@ -45,25 +45,41 @@ begin
     begin
             if (rising_edge(clk)) then
                 if rst = '0' then
-                    xnorn <= "0000000000000001"; --RESET
+                    xnorn <= "00000000000000000000000000000001"; --RESET
                 else
                     if enable = '1' then
-                        xnorn(0) <= xnorn(15) xnor xnorn(13) xnor xnorn(12) xnor xnorn(10); --TAPS
-                        xnorn(1) <= xnorn(0); --DFF
-                        xnorn(2) <= xnorn(1); --DFF
-                        xnorn(3) <= xnorn(2); --DFF
-                        xnorn(4) <= xnorn(3); --DFF
-                        xnorn(5) <= xnorn(4); --DFF
-                        xnorn(6) <= xnorn(5); --DFF
-                        xnorn(7) <= xnorn(6); --DFF
-                        xnorn(8) <= xnorn(7); --DFF
-                        xnorn(9) <= xnorn(8); --DFF
-                        xnorn(10) <= xnorn(9); --DFF
+                        xnorn(0) <= xnorn(31) xnor xnorn(29) xnor xnorn(25) xnor xnorn(24); --TAPS
+                        xnorn(1) <= xnorn(0);   --DFF
+                        xnorn(2) <= xnorn(1);   --DFF
+                        xnorn(3) <= xnorn(2);   --DFF
+                        xnorn(4) <= xnorn(3);   --DFF
+                        xnorn(5) <= xnorn(4);   --DFF
+                        xnorn(6) <= xnorn(5);   --DFF
+                        xnorn(7) <= xnorn(6);   --DFF
+                        xnorn(8) <= xnorn(7);   --DFF
+                        xnorn(9) <= xnorn(8);   --DFF
+                        xnorn(10) <= xnorn(9);  --DFF
                         xnorn(11) <= xnorn(10); --DFF
                         xnorn(12) <= xnorn(11); --DFF
                         xnorn(13) <= xnorn(12); --DFF
                         xnorn(14) <= xnorn(13); --DFF
                         xnorn(15) <= xnorn(14); --DFF
+                        xnorn(16) <= xnorn(15); --DFF
+                        xnorn(17) <= xnorn(16); --DFF
+                        xnorn(18) <= xnorn(17); --DFF
+                        xnorn(19) <= xnorn(18); --DFF
+                        xnorn(20) <= xnorn(19); --DFF
+                        xnorn(21) <= xnorn(20); --DFF
+                        xnorn(22) <= xnorn(21); --DFF
+                        xnorn(23) <= xnorn(22); --DFF
+                        xnorn(24) <= xnorn(23); --DFF
+                        xnorn(25) <= xnorn(24); --DFF
+                        xnorn(26) <= xnorn(25); --DFF
+                        xnorn(27) <= xnorn(26); --DFF
+                        xnorn(28) <= xnorn(27); --DFF
+                        xnorn(29) <= xnorn(28); --DFF
+                        xnorn(30) <= xnorn(29); --DFF
+                        xnorn(31) <= xnorn(30); --DFF
                     else
                         xnorn <= xnorn;       --HOLD
                     end if;
--- work/MISR.vhd
+++ work/MISR.vhd
@@ -29,36 +29,64 @@ entity MISR is
             clk         : in std_logic;
             rst         : in std_logic;
             enable      : in std_logic;
-            mult_r      : in std_logic_vector(7 downto 0);
+            mult_r      : in std_logic_vector(31 downto 0);
     --OUTPUTS
-            signature   : out std_logic_vector(7 downto 0)
+            signature   : out std_logic_vector(31 downto 0)
     );
 end MISR;
 
 architecture Behavioral of MISR is
-	signal xnorn : std_logic_vector(7 downto 0) := "00000001";  --DEFAULT TO 1 because 0 is a NO NO
-	begin
-		process (clk) is
-		begin
-			if (rising_edge(clk)) then
-				if rst = '0' then
-					xnorn <= "00000001"; --DEFAULT TO 1 because 0 is a NO NO
-				else
-				if enable = '1' then
-					xnorn(0) <=    mult_r(0) xnor xnorn(3) xnor xnorn(4) xnor xnorn(5) xnor xnorn(7); --TAPS
-					xnorn(1) <=    mult_r(1) xnor xnorn(0); --DFF with xnor 
-					xnorn(2) <=    mult_r(2) xnor xnorn(1); --DFF with xnor 
-					xnorn(3) <=    mult_r(3) xnor xnorn(2); --DFF with xnor 
-					xnorn(4) <=    mult_r(4) xnor xnorn(3); --DFF with xnor
-					xnorn(5) <=    mult_r(5) xnor xnorn(4); --DFF with xnor 
-					xnorn(6) <=    mult_r(6) xnor xnorn(5); --DFF with xnor 
-					xnorn(7) <=    mult_r(7) xnor xnorn(6); --DFF with xnor 
-				else
-					xnorn <= xnorn; --HOLD
-				end if;
-			end if;
-		end if;
-	end process;
-	signature <= xnorn;
+    signal xnorn : std_logic_vector(31 downto 0) := "00000000000000000000000000000001"; --DEFAULT TO 1 because 0 is a NO NO
+begin
+
+--SYNCHRONOUS shift register
+--with a bit of LFSR pizaz (taps)
+    process (clk) is
+    begin
+            if (rising_edge(clk)) then
+                if rst = '0' then
+                    xnorn <= "00000000000000000000000000000001"; --RESET
+                else
+                    if enable = '1' then
+                        xnorn(0)  <= mult_r(0)  xnor xnorn(31) xnor xnorn(29) xnor xnorn(25) xnor xnorn(24); --TAPS
+                        xnorn(1)  <= mult_r(1)  xnor xnorn(0);  --DFF
+                        xnorn(2)  <= mult_r(2)  xnor xnorn(1);  --DFF
+                        xnorn(3)  <= mult_r(3)  xnor xnorn(2);  --DFF
+                        xnorn(4)  <= mult_r(4)  xnor xnorn(3);  --DFF
+                        xnorn(5)  <= mult_r(5)  xnor xnorn(4);  --DFF
+                        xnorn(6)  <= mult_r(6)  xnor xnorn(5);  --DFF
+                        xnorn(7)  <= mult_r(7)  xnor xnorn(6);  --DFF
+                        xnorn(8)  <= mult_r(8)  xnor xnorn(7);  --DFF
+                        xnorn(9)  <= mult_r(9)  xnor xnorn(8);  --DFF
+                        xnorn(10) <= mult_r(10) xnor xnorn(9);  --DFF
+                        xnorn(11) <= mult_r(11) xnor xnorn(10); --DFF
+                        xnorn(12) <= mult_r(12) xnor xnorn(11); --DFF
+                        xnorn(13) <= mult_r(13) xnor xnorn(12); --DFF
+                        xnorn(14) <= mult_r(14) xnor xnorn(13); --DFF
+                        xnorn(15) <= mult_r(15) xnor xnorn(14); --DFF
+                        xnorn(16) <= mult_r(16) xnor xnorn(15); --DFF
+                        xnorn(17) <= mult_r(17) xnor xnorn(16); --DFF
+                        xnorn(18) <= mult_r(18) xnor xnorn(17); --DFF
+                        xnorn(19) <= mult_r(19) xnor xnorn(18); --DFF
+                        xnorn(20) <= mult_r(20) xnor xnorn(19); --DFF
+                        xnorn(21) <= mult_r(21) xnor xnorn(20); --DFF
+                        xnorn(22) <= mult_r(22) xnor xnorn(21); --DFF
+                        xnorn(23) <= mult_r(23) xnor xnorn(22); --DFF
+                        xnorn(24) <= mult_r(24) xnor xnorn(23); --DFF
+                        xnorn(25) <= mult_r(25) xnor xnorn(24); --DFF
+                        xnorn(26) <= mult_r(26) xnor xnorn(25); --DFF
+                        xnorn(27) <= mult_r(27) xnor xnorn(26); --DFF
+                        xnorn(28) <= mult_r(28) xnor xnorn(27); --DFF
+                        xnorn(29) <= mult_r(29) xnor xnorn(28); --DFF
+                        xnorn(30) <= mult_r(30) xnor xnorn(29); --DFF
+                        xnorn(31) <= mult_r(31) xnor xnorn(30); --DFF
+                    else
+                        xnorn <= xnorn;       --HOLD
+                    end if;
+                end if;
+            end if; 
+        end process;
+    
+signature <= xnorn; --OUTPUT assign
 end Behavioral;
 
