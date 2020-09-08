0a671333664a8a170efb55727150263bb3f1588f
Erez Binyamin <exb4096@rit.edu>
2020-09-08 08:16:26 -0400

    Only add non-uut signals

--- tools/init.tcl
+++ tools/init.tcl
@@ -1,15 +1,24 @@
-# From https://sourceforge.net/p/gtkwave/bugs/23/
-# Script to automatically add all design signals to the wave window on startup;
+# Inspired from: https://gist.github.com/davidzwa/ef1eafc6cd23e613af612e27eddb054b
 set nsigs [ gtkwave::getNumFacs ]
 set sigs [list]
 # fix a strange bug where addSignalsFromList doesn't seem to work
 # if the 1st signal to be added is not a single bit
 lappend sigs "__bug_marker__"
+
+# Add non uut signals to design
 for {set i 0} {$i < $nsigs} {incr i} {
-    set name [ gtkwave::getFacName $i ]
-    lappend sigs $name
+    set signame [ gtkwave::getFacName $i ]
+    set index1 [ string first "uut" $signame  ]
+    if {$index1 == -1} {
+        lappend sigs $signame
+	puts $signame
+    }
 }
 set added [ gtkwave::addSignalsFromList $sigs ]
 
 # Zoom
 gtkwave::/Time/Zoom/Zoom_Full
+
+# Show decimal values
+gtkwave::/Edit/Highlight_All
+gtkwave::/Edit/Data_Format/Signed_Decimal
