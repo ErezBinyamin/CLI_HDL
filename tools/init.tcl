c40a31309771d2a4a03b3d8cc3285790030bebc9
Erez Binyamin <exb4096@rit.edu>
2020-09-17 06:54:47 -0400

    Added .gtkwaverc to change font size, added beautification view options to init.tcl

--- tools/init.tcl
+++ tools/init.tcl
@@ -5,13 +5,14 @@ set sigs [list]
 # if the 1st signal to be added is not a single bit
 lappend sigs "__bug_marker__"
 
-# Add non uut signals to design
+# Add non internal-uut signals to design
+puts "Appending signals:"
 for {set i 0} {$i < $nsigs} {incr i} {
     set signame [ gtkwave::getFacName $i ]
     set index1 [ string first "uut" $signame  ]
     if {$index1 == -1} {
         lappend sigs $signame
-	puts $signame
+	puts "\t$signame"
     }
 }
 set added [ gtkwave::addSignalsFromList $sigs ]
@@ -22,3 +23,11 @@ gtkwave::/Time/Zoom/Zoom_Full
 # Show decimal values
 gtkwave::/Edit/Highlight_All
 gtkwave::/Edit/Data_Format/Signed_Decimal
+gtkwave::/Edit/UnHighlight_All
+
+# Beautification - View settings
+gtkwave::/View/Show_Wave_Highlight
+gtkwave::/View/Show_Filled_High_Values
+gtkwave::/View/Show_Mouseover
+gtkwave::/View/Scale_To_Time_Dimension/None
+
